341337cd995b46d21a78e44f301e831210d5b9de
Hannah <ghdcoalss33@gmail.com>
2025-05-25 20:40:18 +0900

    fix: title, favicon

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,8 +14,8 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro"
+  title: "Melting Factory",
+  description: "Melting Factory - 감정에 언어를 부여하다",
 };
 
 export default function Home() {
